0f2bbc8aa4086e7016f9471a131bb83359fe8919
Camilo <camilovera9811@gmail.com>
2022-09-16 01:25:41 -0500

    termcolor change local to submodule

--- datadl.py
+++ datadl.py
@@ -1,8 +1,12 @@
 from sys import platform
+import sys
 import os
 import shutil
+
+sys.path.insert(1, os.getcwd() + "/" + "/lib/termcolor/src/termcolor")
+from termcolor import colored
 from lib.gdrivedl import gdrivedl as gdl
-from lib.termcolor import colored
+
 
 data_download_link = "https://drive.google.com/drive/folders/1iL811t_-eqnuNwVBGeeU-HG3k6Whd3U9"
 
