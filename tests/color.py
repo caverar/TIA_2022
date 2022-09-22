b6648864f08805ba3bcf06b35cac82fa9a4a317b
Cristian Chitiva <cychitivav@unal.edu.co>
2022-09-21 20:40:36 -0500

    add color segmentation
    change folder name to tests

---
+++ tests/color.py
@@ -0,0 +1,35 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+# Cargamos la imagen
+img_original = cv2.imread('Proc_imagen/Fig/Placa5.jpg')
+img = cv2.cvtColor(img_original, cv2.COLOR_BGR2RGB)
+
+# Convertimos la imagen a HSV
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+img_original = cv2.cvtColor(img_original, cv2.COLOR_BGR2RGB)
+
+# Definimos los colores que queremos detectar   
+h = hsv[:,:,0]
+s = hsv[:,:,1]
+v = hsv[:,:,2]
+
+print(np.max(h), np.min(h))
+
+for i in range(h.shape[0]):
+    for j in range(h.shape[1]):
+        if h[i,j] < 90:
+            img[i,j] = 0
+
+# Mostramos la imagen
+plt.subplot(2,2,1)
+plt.imshow(img_original)
+
+plt.subplot(2,2,2)
+plt.imshow(img)
+
+plt.subplot(2,1,2)
+plt.imshow(cv2.cvtColor(img, cv2.COLOR_RGB2GRAY), cmap='gray')
+
+plt.show()
